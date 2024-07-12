5f2a74f40a5b60b776036d1664330d26ac488948
“Vustey” <vutv579@gmail.com>
2024-07-12 21:46:38 +0700

    validation email and password

--- models/User.js
+++ models/User.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6
+    required: [true, "Please enter a password"],
+    minLength: [6, "Minimum password length is 6 characters"],
   },
 });
 
-
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
